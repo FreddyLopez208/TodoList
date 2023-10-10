26f9b5e94fd9de6b5aa063562d962f7573df2492
Freddy Lopez <freddy.lopez102@alu.ulpgc.es>
2023-10-10 21:18:50 +0100

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Freddy Alexander López Gutiérrez on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
